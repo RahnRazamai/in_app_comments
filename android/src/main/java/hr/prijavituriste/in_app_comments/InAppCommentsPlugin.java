ef6f42e5814fa72a2dd6e10733f0d1b34d5ecd9e
Rahn Qarnain R Razamai <rr.rahnrazamai@gmail.com>
2025-02-27 13:46:25 +0800

    Update InAppCommentsPlugin.java

--- android/src/main/java/hr/prijavituriste/in_app_comments/InAppCommentsPlugin.java
+++ android/src/main/java/hr/prijavituriste/in_app_comments/InAppCommentsPlugin.java
@@ -12,20 +12,13 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 
 /** InAppCommentsPlugin */
-public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
-  /// The MethodChannel that will the communication between Flutter and native
-  /// Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine
-  /// and unregister it
-  /// when the Flutter Engine is detached from the Activity
+public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, ActivityResultListener {
   private MethodChannel channel;
   private Result result;
-  Activity activity;
+  private Activity activity;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -37,10 +30,13 @@ public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, Ac
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     this.result = result;
     if (call.method.equals("requestComments")) {
+      if (activity == null) {
+        result.error("NO_ACTIVITY", "Activity is null", null);
+        return;
+      }
       Intent intent = new Intent("com.huawei.appmarket.intent.action.guidecomment");
       intent.setPackage("com.huawei.appmarket");
       activity.startActivityForResult(intent, 1001);
-      //result.success(0);
     } else {
       result.notImplemented();
     }
@@ -52,49 +48,63 @@ public class InAppCommentsPlugin implements FlutterPlugin, MethodCallHandler, Ac
   }
 
   @Override
-public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
     activity = activityPluginBinding.getActivity();
     activityPluginBinding.addActivityResultListener(this);
-}
-
-@Override
-public void onDetachedFromActivityForConfigChanges() {
-  activity = null;
-}
-
-@Override
-public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
-  activity = activityPluginBinding.getActivity();
-  activityPluginBinding.addActivityResultListener(this);
-}
-
-@Override
-public void onDetachedFromActivity() {
-  activity = null;
-}
-
-@Override
-public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == 1001) {
-        if (resultCode == 102 || resultCode == 103) {
-          result.success(resultCode);
-        } else if (resultCode == 0) {
-          result.error("0", "error", "Unknown error");
-        } else if (resultCode == 101) {
-          result.error("101", "error", "The app has not been released on AppGallery");
-        } else if (resultCode == 104) {
-          result.error("104", "error", "The HUAWEI ID sign-in status is invalid");
-        } else if (resultCode == 105) {
-          result.error("105", "error", "The user does not meet the conditions for displaying the comment pop-up");
-        } else if (resultCode == 106) {
-          result.error("106", "error", "The commenting function is disabled");
-        } else if (resultCode == 107) {
-          result.error("107", "error", "The in-app commenting service is not supported");
-        } else if (resultCode == 108) {
-          result.error("108", "error", "The user canceled the comment");
-        }
-        return true;
   }
-  return false;
-}
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    activity = null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+    activity = activityPluginBinding.getActivity();
+    activityPluginBinding.addActivityResultListener(this);
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    activity = null;
+  }
+
+  @Override
+  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == 1001 && result != null) {
+      switch (resultCode) {
+        case 102:
+        case 103:
+          result.success(resultCode);
+          break;
+        case 0:
+          result.error("0", "error", "Unknown error");
+          break;
+        case 101:
+          result.error("101", "error", "The app has not been released on AppGallery");
+          break;
+        case 104:
+          result.error("104", "error", "The HUAWEI ID sign-in status is invalid");
+          break;
+        case 105:
+          result.error("105", "error", "The user does not meet the conditions for displaying the comment pop-up");
+          break;
+        case 106:
+          result.error("106", "error", "The commenting function is disabled");
+          break;
+        case 107:
+          result.error("107", "error", "The in-app commenting service is not supported");
+          break;
+        case 108:
+          result.error("108", "error", "The user canceled the comment");
+          break;
+        default:
+          result.error(String.valueOf(resultCode), "Unknown error code", null);
+          break;
+      }
+      result = null; // Clear the result reference after handling it
+      return true;
+    }
+    return false;
+  }
 }
